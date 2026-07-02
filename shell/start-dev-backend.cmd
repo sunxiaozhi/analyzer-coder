c26043f3e29187760f0db0fa5f211b7d14e84bcc
sunxiaozhi <1129535445@qq.com>
2026-07-02 08:25:32 +0800

    docker compose

--- shell/start-dev-backend.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set ANALYZER_MYSQL_USER=analyzer_coder
-set ANALYZER_MYSQL_PASSWORD=analyzer_coder
-set ANALYZER_MYSQL_DATABASE=analyzer_coder
-cd /d C:\Users\admin\Desktop\coder
-C:\Users\admin\Desktop\coder\.venv\Scripts\python.exe -m web.backend.app
-pause
--- shell/start-dev-frontend.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\admin\Desktop\coder\web\frontend
-npm run dev
-pause
